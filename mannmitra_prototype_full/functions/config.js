87bf4f8cc55de0cbbe405042d93cb3e0c108f138
Alpeshkumar Thaker <20251603002@iiitvadodara.ac.in>
2025-09-20 08:16:23 +0530

    Update config.js

--- mannmitra_prototype_full/functions/config.js
+++ mannmitra_prototype_full/functions/config.js
@@ -1,8 +1,8 @@
 exports.HELPLINES = [
-  { name: "iCall (TISS)", number: "+91 0000000000" },
-  { name: "Vandrevala Foundation Helpline", number: "+91 0000000000" },
-  { name: "Snehi (Delhi)", number: "+91 0000000000" },
-  { name: "Institution (Dummy - earlier Gandhinagar IIITV)", number: "+91 0000000000" }
+  { name: "Online Support (TISS)", number: "+91 98797096.." },
+  { name: "Helpline", number: "+91 98797096.." },
+  { name: "Snehi (Gandhinagar)", number: "+91 98797096.." },
+  { name: "Institution (Gandhinagar IIITV)", number: "+91 98797096.." }
 ];
 
 exports.CRISIS_KEYWORDS = [
